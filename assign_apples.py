82badd0769c6cb088bb0a785a249b0b55f5e1451
rmorros <ramon.morros@upc.edu>
2025-03-17 16:20:23 +0100

    Initial commit

---
+++ assign_apples.py
@@ -0,0 +1,96 @@
+from functions.utils import stretch_velocity, read_csv, get_change_frames, assign_all_apples, write_results, get_gt_list
+import argparse
+import os
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Evaluate detection')
+
+    parser.add_argument('--data_path', dest='data_path',
+                        help='Path where the subdirectories generated when executing the FruitTracker_inference.py'
+                             'file are located.')
+    parser.add_argument('--sframe_file', dest='sframe_file',
+                        help='Path to the stretch frames "csv" file. Not needed in case of SGNSS mode.', required=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    data_path = args.data_path
+    sframe_file = args.sframe_file
+
+
+    velocity = data_path.split('/')[-1]
+
+    exec_mode = str
+    camera = str
+    gt_list = []
+    if 'KA' in data_path:
+        camera = 'KA'
+        exec_mode = 'SFRAME'
+    else:
+        camera = 'ZED'
+        if 'GNSS' in data_path:
+            exec_mode = 'SGNSS'
+        else:
+            exec_mode = 'SFRAME'
+
+    print ('Configured options:')
+    print (f'Camera: {camera}, exec mode: {exec_mode}, SFRAME file: {sframe_file}')
+    for file in os.listdir(data_path):
+        print(file)
+        if exec_mode == 'SGNSS':
+            gps_path = os.path.join(data_path, file, 'frame_coordinates.csv')
+        else:
+            gps_path = None
+
+        csv_path = os.path.join(data_path, file, 'all_predictions.csv')
+        gt_list = get_gt_list(csv_path, exec_mode, sframe_file)
+        all_apples = read_csv(csv_path)
+
+        # assigned_apples is a list with all the apples that have been assigned to a stretch
+        # stretch_velocities contains the mean velocity of all the apples that appears in the changing frame
+        # Then, the mean velocity for each frame can be known
+        assigned_apples, stretch_velocities, id_ass_apples = stretch_velocity(all_apples=all_apples, gt_list=gt_list)
+
+        # Get the list of tuples, where for each stretch change we obtain from which frame to which frame we are at the
+        # moment of the change.
+        frames_changing = get_change_frames(gt_list=gt_list, stretch_velocities=stretch_velocities)
+
+        print ('Entering function assign_all_apples(). This may take a while ...')
+
+        apples_stretch = assign_all_apples(frames_changing=frames_changing, assigned_apples=assigned_apples,
+                                           all_apples=all_apples, id_ass_apples=id_ass_apples,
+                                           stretch_velocities=stretch_velocities)
+
+        # Get the name of the video, from the path of csv data
+        video_name = csv_path.split(os.path.sep)[-2]
+        video_orientation = video_name[-13:-12]
+
+        # Create folder with the video names to store the results
+        # path_to_results = os.path.join('results_stretch', camera, exec_mode, file)
+        path_to_results = os.path.join('results_stretch', camera, exec_mode, velocity, file)
+        os.makedirs(os.path.dirname(path_to_results), exist_ok=True)
+
+        path_to_stretch_num = os.path.join(path_to_results, 'all_apples.csv')
+        path_to_all_apples = os.path.join(path_to_results, 'apples_stretch.csv')
+        path_to_all_predictions = os.path.join(path_to_results, 'all_predictions.csv')
+        path_to_gps = os.path.join(path_to_results, 'frame_coordinates.csv')
+
+        # If the video is shot from west, apply a transformation in order to invert the stretch number, so the stretch
+        # 1 will be 21, 2 -> 20, etc...
+        if video_orientation == 'w':
+            assigned_apples = [[sublist[0], 22 - sublist[1]] for sublist in assigned_apples]
+            write_results(assigned_apples, path_to_all_apples, path_to_stretch_num, csv_path, path_to_all_predictions,
+                          path_to_gps, gps_path, exec_mode)
+        else:
+            write_results(apples_stretch, path_to_all_apples, path_to_stretch_num, csv_path, path_to_all_predictions,
+                          path_to_gps, gps_path, exec_mode)
+
+
+if __name__ == "__main__":
+    main()
---
+++ fruit_tracker_simple.py
@@ -0,0 +1,302 @@
+import sys
+import os
+import numpy as np
+import cv2
+import argparse
+from pathlib import Path
+import pandas as pd
+from bisect import bisect
+
+
+from tracking.FruitTracker_simple import FruitTracker
+from functions.functions import get_all_detections, perform_tracking, write_results_simple
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Evaluate detection')
+
+    parser.add_argument('--video', dest='video', help='Video file path.')
+    parser.add_argument('--segments-file', dest='segments_file',
+                        help='File containing the limits of the segments. For each video, the segment limits'
+                             ' are defined at the frame where the segment change line is centered on the frame.')
+    parser.add_argument('--weights', dest='weights', help='Weights file for the YOLOv5 detection model')
+    parser.add_argument('--offset', dest='offset', default=0, help='Number of frames to skip at the beginning.')
+    parser.add_argument('--conf', dest='confidence', default=0.25,
+                        help='Minimum detection confidence to be taken into account.')
+    parser.add_argument('--min_area', dest='min_area', default='-1',
+                        help='Min area (in pixels) of the apples to be tracked. If -1, the value will be determined'
+                        'from the seventh field of the video file name (distance).')
+
+    parser.add_argument('--data', type=str, default='data/apple_segmentation.yaml', help='dataset.yaml path')    
+    parser.add_argument('--camera', dest='camera', default='Agnostic', help='Camera type (ZED or KA). Only necessary for the provided videos.')
+
+    parser.add_argument('--rotate', dest='rotate', default=False, help='Whether to rotate each frame 90º clockwise. Useful when camera has been situated vertically')
+    parser.add_argument('--results_file', dest='predictions_filename', default="all_predictions.csv", help='Filename for the output tracking results')
+    
+    args = parser.parse_args()
+
+    return args
+
+
+
+def configure_min_area_filter_from_filename (camera_name, distance):
+    '''
+    Return the minimum area size for the configured presets:
+      - Camera: ZED of KA
+      - Recording distances: 125, 175, 225
+    '''
+
+    if camera_name =='Agnostic':
+        print ('Error: This funcion is only intended for Kinect or ZED cameras')
+        sys.exit()
+        
+    preset_areas = {'KA':{225: 100, 175:221, 125: 624}, 'ZED':{225:77, 175:500, 125:120}}
+    if distance not in [125,175,225]:
+        print ('ERROR: valid distances for the provided data are 125, 175 and 225')
+        sys.exit(1)
+    return preset_areas[camera_name][distance]
+    
+
+def get_stretch_changes_simple(video_path, segments_file):
+    """
+    Get a list that contains all the stretch changes for the current video
+
+    Parameters:
+        - video_path: Path to the video being processed
+         - segment_file: File with the stretch locations (frames) in video
+
+    Returns:
+        - stretch_changes: list with all the stretch changes
+    """
+
+    df_stretch = pd.read_csv(segments_file, header=None)
+
+    # Trim any trailing spaces in the names column                                                                                                   
+    df_stretch = df_stretch.map(lambda x: x.strip() if isinstance(x, str) else x)
+
+    # Get the row corresponding to the current video                                                                                                 
+    mask = df_stretch[0].isin([video_path.stem])
+
+    # Return a list with the stretch_changes for the current video                                                                                   
+    return df_stretch[mask].values.flatten()[1:]    
+
+
+import torch
+import torch.backends.cudnn as cudnn
+
+# pip install yolov5
+from yolov5.utils.torch_utils import select_device
+from yolov5.models.common import DetectMultiBackend
+from yolov5.utils.augmentations import letterbox
+from yolov5.utils.general import check_img_size, non_max_suppression, scale_boxes
+
+class yolo_detector:
+    def __init__(self,
+                 weights='data/last.pt', # use trained PT file
+                 imgsz=(640, 640), # Mod
+                 data='data/apple_segmentation.yaml',
+                 conf_thres=0.25,
+                 iou_thres=0.45,
+                 half=False,
+                 dnn=False,
+                 max_det=1000,
+                 ):
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+        self.max_det = max_det
+
+        self.device = select_device('') # Mod
+        self.model = DetectMultiBackend(weights, device=self.device, dnn=dnn, data=data, fp16=half) # load the model
+        self.stride, pt = self.model.stride, self.model.pt
+        self.imgsz = check_img_size(imgsz, s=self.stride)  # check image size
+        half &= pt and self.device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+        self.half = half
+        if pt:
+            self.model.model.half() if half else self.model.model.float()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        self.model.warmup(imgsz=(1, 3, *self.imgsz))
+
+
+
+        
+    def run(self, frame):
+        print (frame.shape)
+        
+        img = letterbox(frame, self.imgsz, stride=self.stride, auto=True)[0]
+
+        img = img.transpose((2, 0, 1))
+        img = np.ascontiguousarray(img)
+
+        im = torch.from_numpy(img).to(self.device)
+        im = im.half() if self.half else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+
+        print (im.shape)
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        pred = self.model(im, augment=False, visualize=False)
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, max_det=self.max_det)
+
+        to_return_bbox = []
+        to_return_scores = []
+        for i, det in enumerate(pred):
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], frame.shape).round()
+
+            for *xyxy, conf, cls in reversed(det):
+                    bbox = (torch.tensor(xyxy).view(1, 4)).view(-1).tolist()
+                    w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
+                    bbox[2], bbox[3] = w, h
+                    to_return_bbox.append(bbox)
+                    to_return_scores.append(conf.item())
+        return to_return_bbox, to_return_scores
+
+
+def main():
+    args = parse_args()
+
+    video_path = Path(args.video)
+    segments_file = args.segments_file
+    confidence = float(args.confidence)
+    offset = int(args.offset)
+    weights = args.weights
+    min_area = int(args.min_area)
+    camera = args.camera
+    rotate = args.rotate
+    data = args.data
+    csv_all_predictions_filename = args.predictions_filename
+
+    
+    # Initialize variables
+    ii = offset
+
+    if camera not in ['Agnostic', 'ZED', 'KA']:
+        print ('Error: Incorrect camera type. If unsure, use Agnostic')
+        sys.exit()
+        
+    if camera == 'Agnostic' and min_area == -1:
+        print ('ERROR!: When using agnostic camera, the min_area must be splicitly provided')
+        sys.exit()
+        
+    if min_area == -1:
+        # The distance from camera to the trees is encoded in the video name
+        distance = int(os.path.split(video_path.stem)[1].split('_')[6])
+        min_area = configure_min_area_filter_from_filename(camera, distance)
+
+    # ???????
+    if offset != 0:
+        ii = offset + 1
+
+    
+    # get the information of the segments change
+    stretch_changes = get_stretch_changes_simple(video_path, segments_file)
+
+    
+    # Parameters for detector configuration
+    imgsz = [640]
+    imgsz *= 2 if len(imgsz) == 1 else 1
+    conf_thres = confidence
+    iou_thres = 0.45
+    half = False
+    dnn = False
+    max_det = 1000
+
+    detector = yolo_detector(weights, imgsz, data, conf_thres, iou_thres, half, dnn, max_det)
+
+    fruit_tracker = FruitTracker(img_sz=imgsz, min_area=min_area)
+
+    # List used to find the value of each stretch  ???????
+    list_to_compare = list(range(1, 22))
+    reverse_list = list(range(21, 0, -1))
+
+    # List with the coordinates of each frame
+    frame_coordinates_list = []
+
+    # List with all the results of tracking, Saved on CSV
+    # all_tracking_predictions = [[Number of Frame, ID1, x_min, y_min, w, h, Score, Stretch],
+    #                            [Number of Frame, ID2, x_min, y_min, w, h, Score, Stretch]], ...]
+    all_tracking_predictions = []
+
+    # List with the coordinates of each frame
+    frame_coordinates_list = []
+
+    prev_stretch    = 0
+    frames_interval = ii
+
+
+    cap = cv2.VideoCapture(args.video)
+    frame_counter = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        # Skip the first frames. Note that while cap.set(cv2.CAP_PROP_POS_FRAMES, offset) can also be used for that, 
+        # actually reading and discarting the frames is safer and probably faster as well.
+        if frame_counter < ii:
+            continue
+
+        stretch = bisect(stretch_changes, ii)
+
+        
+        frame_counter = frame_counter + 1
+                
+        print (f'Processing frame f{frame_counter}')
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        if (rotate):
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+        dets_conf = get_all_detections(detector, frame)
+        
+        # Pass the detections, number of frame and frame(np.array) to the tracker, also pass camera name
+        fruit_tracker.get_detections(dets_conf, ii, frame, stretch)
+        
+        # Perform the tracking and save the results, new_predictions = last predictions returned by tracker
+        tracking_predictions = fruit_tracker.track_yolo_results()
+
+        new_predictions = tracking_predictions[-1]
+        
+
+
+        if camera != 'Agnostic':
+            # If the video is shot from west, apply a transformation in order to invert the stretch number, so the stretch
+            # 1 will be 21, 2 -> 20, etc...
+            
+            # Extract the orientation (west or east) from which the video was shot. For the provided videos this is encoded
+            # in the filename
+            video_orientation = video_path.stem.split('_')[4]
+            
+            if video_orientation == 'w':
+                stretch_index = bisect(list_to_compare, stretch)
+                if stretch_index == 0:
+                    stretch = 21
+                else:
+                    stretch = reverse_list.index(stretch_index)
+
+                #if str(video_path)[-17: -16] == 'w':  # JRMR: was in perform_tracking()
+                stretch += 1
+
+        all_tracking_predictions = perform_tracking(new_predictions=new_predictions,
+                                                    all_tracking_predictions=all_tracking_predictions,
+                                                    stretch=stretch, i=ii)
+
+
+        ii += 1
+
+    write_results_simple(csv_all_predictions_filename=csv_all_predictions_filename,
+                         all_tracking_predictions=all_tracking_predictions)
+
+        
+    cap.release()
+
+if __name__ == "__main__":
+    main()
+
+    
+    
+            
